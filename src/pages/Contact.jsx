e1df81b3ff24751ae97464db1aa3c31e65501e31
NwabKhan <m.junaid.89630@gmail.com>
2023-02-27 23:07:43 +0500

    added contact pages using formspree library

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 const Contact = () => {
-  const Wrapper = styled.section`
+  const Wrapper = styled.div`
     padding: 9rem 0 5rem 0;
     text-align: center;
 
@@ -33,7 +33,30 @@ const Contact = () => {
     }
   `;
 
-  return <div>Contacts</div>;
+  return (
+    <Wrapper>
+      <h2 className="common-heading">Contact Page</h2>
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d26587.074588806536!2d73.03401030380681!3d33.59533136428672!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38df94834d87f5a3%3A0x506e17bedd22f2e7!2sSaddar%2C%20Rawalpindi%2C%20Punjab%2046000%2C%20Pakistan!5e0!3m2!1sen!2s!4v1677518783032!5m2!1sen!2s"
+        width="100%"
+        height="350"
+        style={{border:0}}
+        allowFullScreen=""
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+      ></iframe>
+      <div className="container">
+        <div className="contact-form">
+          <form action="https://formspree.io/f/mwkjbprr" method="POST" className="contact-inputs">
+            <input type="text" placeholder="Username" name="Username" required autoComplete="off" />
+            <input type="email" placeholder="Email" name="Email" required autoComplete="off" />
+            <textarea type = "text" name="Message" placeholder="Write your message" required autoComplete="off" cols="30" rows='10' ></textarea>
+            <input type="submit" value="Send" />
+          </form>
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default Contact;
